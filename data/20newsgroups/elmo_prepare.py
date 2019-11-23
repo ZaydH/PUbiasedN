ed0305912ac32434e56b0e7b48dd6416a55534dd
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2019-11-23 01:42:30 -0800

    Fixed and made the 20 newsgroups vector generator more robust and stable

--- data/20newsgroups/elmo_prepare.py
+++ data/20newsgroups/elmo_prepare.py
@@ -1,6 +1,6 @@
 import sys
 import h5py
-import allennlp.commands
+from allennlp.commands.elmo import ElmoEmbedder
 import numpy as np
 import nltk
 import torch
@@ -9,38 +9,42 @@ from nltk.tokenize import word_tokenize
 from sklearn.datasets import fetch_20newsgroups
 
 WEIGHT_FILE = "https://allennlp.s3.amazonaws.com/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5"
+OPTIONS_FILE = "https://allennlp.s3.amazonaws.com/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json"
 
 
 def generate_dataset(ds_name):
     assert ds_name == "train" or ds_name == "test"
-    newsgroups = fetch_20newsgroups(subset=ds_name)
+    newsgroups = fetch_20newsgroups(subset=ds_name, shuffle=True)
 
     n = len(newsgroups.data)
 
-    elmo = allennlp.commands.elmo.ElmoEmbedder(
-        'elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json',
-        cached_path(WEIGHT_FILE, "."), 0 if torch.cuda.is_available() else -1)
+    def _make_elmo(n_device: int) -> ElmoEmbedder:
+        return ElmoEmbedder(cached_path(OPTIONS_FILE, "."), cached_path(WEIGHT_FILE, "."), n_device)
 
+    # First learner use CUDA, second does not
+    elmos = [_make_elmo(i) for i in range(0, -2, -1)]
     data = np.zeros([n, 9216])
-    f = h5py.File(f'20newsgroups_elmo_mmm_{ds_name}.hdf5', 'w')
-    f.create_dataset('data', data=data)
-    f.close()
 
     if not torch.cuda.is_available():  # Has to be out of for loop or stdout overwrite messes up
         print('cuda fail')
     for i in range(n):
-        A = word_tokenize(newsgroups.data[i])
+        A = [word_tokenize(newsgroups.data[i])]
         sys.stdout.write(f"Processing {ds_name} document {i+1}/{n}\r")
         sys.stdout.flush()
-        em = elmo.embed_batch([A])
+        try:
+            em = elmos[0].embed_batch(A)
+        except RuntimeError:
+            em = elmos[1].embed_batch(A)
         em = np.concatenate(
                 [np.mean(em[0], axis=1).flatten(),
                  np.min(em[0], axis=1).flatten(),
                  np.max(em[0], axis=1).flatten()])
-        f = h5py.File(f'20newsgroups_elmo_mmm_{ds_name}.hdf5', 'r+')
-        f['data'][i] = em
-        f.close()
+        data[i] = em
     print("")
+
+    f = h5py.File(f'20newsgroups_elmo_mmm_{ds_name}.hdf5', 'w')
+    f.create_dataset('data', data=data)
+    f.close()
 
 
 if __name__ == "__main__":
